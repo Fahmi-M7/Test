a9e61068f4b6c947606e3ae78fdee8fda751442a
Fahmi Rahmat Azis <fahmi.fra.work@gmail.com>
2022-08-12 18:27:17 +0700

    edit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
-function App() {
-  cons
+import ProductList from "./components/ProductList";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import AddProduct from "./components/AddProduct";
+import EditProduct from "./components/EditProduct";
+
+function App(){
   return (
-    <div>
-      <h1>Hello World</h1>
+    <div className="container">
+      <div className="columns">
+        <div className="column is-half is-offset-one-quarter">
+          <Router>
+            <Routes>
+              <Route path="/" element={<ProductList />}/>
+              <Route path="/add" element={<AddProduct />}/>
+              <Route path="/edit:id" element={<EditProduct/>}/>
+            </Routes>
+          </Router>
+        </div>
+      </div>
     </div>
   );
 }
-
 export default App;
+
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+const Header = () => {
+    return(
+        <div>
+            <h2>Ini adalah component header</h2>
+        </div>
+    )
+}
+export default Header
